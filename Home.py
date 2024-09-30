0def177a15af3f3372d949eff0babe899971ebc1
Amalya <88465999+amalyacox@users.noreply.github.com>
2024-09-29 17:46:15 -0700

    initial updates

---
+++ Home.py
@@ -0,0 +1,37 @@
+
+# Main Page of Website 
+
+import streamlit as st 
+import streamlit.components.v1 as components 
+from details import * 
+
+st.set_page_config(page_title="Main Page", page_icon="🏠", layout="wide") 
+
+# sidebar --------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+with st.sidebar:
+    st.success("Select a page above.")
+
+# main page --------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+st.header("Amalya Johnson", divider='rainbow')
+
+col1, col2, col3 = st.columns([1.3, 0.2, 1])
+
+with col1: 
+    st.markdown(info['brief'])
+
+
+col4, col5 = st.columns([0.1,3])
+
+with col4: 
+    st.markdown(linkedin_logo, unsafe_allow_html=True)
+    st.markdown(github_logo, unsafe_allow_html=True)
+    st.markdown(research_logo, unsafe_allow_html=True)
+    # st.markdown(research_logo, unsafe_allow_html=True)
+
+with col5: 
+    st.markdown(f"###### [Linkedin]({linkedin_link})")
+    st.markdown(f"###### [Github]({github_link})")
+    st.markdown(f"###### [Google Scholar]({googlescholar_link})")
+
+
---
+++ details.py
@@ -0,0 +1,46 @@
+
+
+#publication_url --------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+linkedin_logo = '''                                                                                                                                          
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
+  <i class="fa-brands fa-linkedin" style="font-size: 28px;"></i>                                                                           
+'''
+
+github_logo = '''
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
+  <i class="fa-brands fa-github" style="font-size: 28px;"></i>                                                                           
+'''
+
+research_logo = '''
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
+    <i class="fa-solid fa-book" style="font-size: 28px;"></i>
+'''
+
+googlescholar_logo = '''
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
+    <i class="fa-brands fa-google-scholar" style="font-size: 28px;"></i>
+'''
+
+### linkes 
+linkedin_link = 'https://www.linkedin.com/in/amalya-johnson-3a1b13156/'
+github_link = 'https://github.com/amalyacox'
+googlescholar_link = 'https://scholar.google.com/citations?user=bUNsfjkAAAAJ&hl=en&oi=ao'
+
+# personal info (for main page) -----------------------------------------------------------------------------------------------------------------------------------------------------------------
+info = {'brief':
+              """ 
+            
+Hello! And welcome to my website. 
+
+I am a Materials Science & Engineering PhD candidate at Stanford University studying the thermal and excitonic properties of atomically thin or "two-dimensional" (2D) materials. 
+I am advised by Professors [Fang Liu](https://sfrlab.netlify.app/) and [Aaron Lindenberg](https://lindenberglab.stanford.edu/), and heavily collaborate with Dr. [Archana Raja](https://foundry.lbl.gov/about/staff/archana-raja/) at Lawrence Berkeley National Laboratory. 
+I earned my Bachelors in Physics from Columbia University, where I was a [4-year starter](https://www.columbiaspectator.com/sports/2019/11/12/one-of-the-best-players-in-the-history-of-this-program-amalya-johnson-ends-a-career-filled-with-many-accolades-and-even-more-memories/) 
+and senior captain of the Women's Soccer Team.
+
+              """,
+        # 'name':'', 
+        # 'study':'',
+        # 'location':'',
+        # 'interest':'',
+        # 'skills':['Python'],
+        }
